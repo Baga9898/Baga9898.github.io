039adaeb7d45f19d03036af0f1c14a285a1a4b31
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2020-03-04 21:49:16 +0300

    burger menu on main page

--- js/style.js
+++ js/style.js
@@ -1,26 +1,7 @@
-function burgerMenu(selector) {
-    let menu = $(selector);
-    let button = menu.find('.burger-menu__button');
-    let links = menu.find('.burger-menu__link');
-    let overlay = menu.find('.burger-menu__overlay');
+const menuToggle = document.querySelector('#menu-toggle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
 
-    button.on('click', (e) => {
-        e.preventDefault();
-        toggleMenu();
-    });
-
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
-
-    function toggleMenu() {
-        menu.toggleClass('burger-menu_active');
-
-        if (menu.hasClass('burger-menu__active')) {
-            $('body').css('overflow', 'hidden');
-        } else {
-            $('body').css('overflow', 'visible');
-        }
-    }
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active');
 }
-
-burgerMenu('.burger-menu');
