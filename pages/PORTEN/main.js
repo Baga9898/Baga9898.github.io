6c54cb33120dfa64220f33bb27466e0594eacb0d
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2020-12-28 13:14:57 +0300

    JSopenModalWindow/ShowAnimation

--- pages/PORTEN/main.js
+++ pages/PORTEN/main.js
@@ -5,8 +5,15 @@ var login = popup.querySelector("[name=login]");
 var form = popup.querySelector("form");
 var password = popup.querySelector("[name=password]");
 var storage = localStorage.getItem("login");
+
 var isStorageSupport = true;
 var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+} catch (err) {
+    isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
@@ -23,12 +30,15 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classlist.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
     if (!login.value || !password.value) {
         evt.preventDefault();
-        console.log("Необходимо ввести логин и пароль");
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
     } else {
         if (isStorageSupport) {
             localStorage.setItem("login", login.value);
@@ -36,8 +46,13 @@ form.addEventListener("submit", function (evt) {
     }
 });
 
-try {
-    storage = localStorage.getItem("login");
-} catch (err) {
-    isStorageSupport = false;
-}
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode == 27) {
+        if (popup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
