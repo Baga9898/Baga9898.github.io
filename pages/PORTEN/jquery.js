d78ea9a8ab0a3809887c62aeedf57d5c8341dc1e
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2020-12-30 22:59:24 +0300

    BurgerMenuFull

--- pages/PORTEN/jquery.js
+++ pages/PORTEN/jquery.js
@@ -3,3 +3,15 @@ $(function() {
         $('.menu__buttons').toggleClass('show-menu')
     })
 });
+
+$(function() {
+    $('.menu-open').click(function() {
+        $('.cross__close').addClass('cross__active')
+    })
+});
+
+$(function() {
+    $('.cross__close').click(function() {
+        $('.menu__buttons').toggleClass('show-menu')
+    })
+});
