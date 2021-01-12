26efcaae129ebdf8de6791402c8575a207fed47a
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2021-01-12 12:20:45 +0300

    FullRestructure

--- js/style.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-toggle');
-const mobileNavContainer = document.querySelector('#mobile-nav');
-
-menuToggle.onclick = function() {
-    menuToggle.classList.toggle('menu-icon-active');
-    mobileNavContainer.classList.toggle('mobile-nav--active');
-}
