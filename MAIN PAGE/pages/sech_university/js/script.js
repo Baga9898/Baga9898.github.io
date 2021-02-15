7e587838e5ded8bb53a4e6df2ec7b5f52f6e68f4
Baga9898 <61385284+Baga9898@users.noreply.github.com>
2021-02-15 12:09:00 +0300

    Fix

--- MAIN PAGE/pages/sech_university/js/script.js
+++ MAIN PAGE/pages/sech_university/js/script.js
@@ -1,7 +1,35 @@
 jQuery(document).ready(function() {
 
+    // AlertHide
+
     $('.alert-button').click(function() {
         $('.alert-new').removeClass('active')
+    });
+
+    // Counter
+
+    $('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({ countNum: $this.text() }).animate({
+                countNum: countTo
+            },
+
+            {
+
+                duration: 8000,
+                easing: 'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+
     });
 
 });
